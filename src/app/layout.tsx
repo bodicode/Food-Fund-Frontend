bbbca4d2624ad91b97a477f825044a8f972c57ea
Bo <29.hoang.10@gmail.com>
2025-09-22 14:59:16 +0700

    fix: add layout for not found page

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../app/(main)/globals.css";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export const metadata: Metadata = {
+  title: "FoodFund",
+  description: "FoodFund is a platform for funding food projects",
+  icons: "/images/favicon.ico",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
